ec18dd8b7d60b31189356f79bd94c475860c1fcc
Nurmuhammad-TUIT <nurmuhammad09062020@gmail.com>
2022-02-19 14:38:43 +0300

    change html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 alert ("Welcome");
 
 var firstName = prompt ("Ismingizni kiriting");
-var firstQuestion = prompt ("Node js nima ?");
-var secondQuestion = prompt ("NPM - kengaytmasini kiriting");
+
+var firstQuestion = prompt ("Bootstrap o`zi nima");
+var secondtQuestion = prompt ("Caruselda img ni o`rab turgan divni calssi qanday ?"); //carusel item
 var thirdQuestion = prompt ("Bootstrapda yuqoridan va pastdan margin berish qanday bajariladi?");
-var fourthQuestion = prompt ("Bootstrap o`zi nima");
+var fourthQuestion = prompt ("NPM - kengaytmasini kiriting");
 var fivethQuestion = prompt ("Bootstrapda inputga birinchi navbatda qanday class berish kerak?");
-var sixthQuestion = prompt ("Card class ichida nechta class lar mavjud?");
+var sixthQuestion = prompt ("Card class ichida nechta class lar mavjud?"); 
 var seventhQuestion = prompt ("Modal saytni qayerida turishi kerak?");
 var eightthQuestion = prompt ("Chapdan padding qanday beriladi?");
 var ninethQuestion = prompt  ("Input-group class ini  qaysi tag ga berish kerak?" );
