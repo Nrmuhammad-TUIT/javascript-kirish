605366ab810711bd406eae71faa96ef22ad28f47
Nurmuhammad-TUIT <nurmuhammad09062020@gmail.com>
2022-02-19 10:50:47 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+alert ("Welcome");
+
+var firstName = prompt ("Ismingizni kiriting");
+var firstQuestion = prompt ("Node js nima ?");
+var secondQuestion = prompt ("NPM - kengaytmasini kiriting");
+var thirdQuestion = prompt ("Bootstrapda yuqoridan va pastdan margin berish qanday bajariladi?");
+var fourthQuestion = prompt ("Bootstrap o`zi nima");
+var fivethQuestion = prompt ("Bootstrapda inputga birinchi navbatda qanday class berish kerak?");
+var sixthQuestion = prompt ("Card class ichida nechta class lar mavjud?");
+var seventhQuestion = prompt ("Modal saytni qayerida turishi kerak?");
+var eightthQuestion = prompt ("Chapdan padding qanday beriladi?");
+var ninethQuestion = prompt  ("Input-group class ini  qaysi tag ga berish kerak?" );
+//div ga  
+var tenthQuestion = prompt ("Ekranni 4 ta bo`lakka bo`lish uchun col-x, x ni kiriting")
+
+console.log(firstName + "\n" + firstQuestion +"\n" + secondQuestion + "\n"
+ + thirdQuestion +  "\n" + fivethQuestion + "\n" + sixthQuestion + "\n" 
+ + seventhQuestion + "\n" + eightthQuestion + "\n" + ninethQuestion + "\n" + tenthQuestion );
